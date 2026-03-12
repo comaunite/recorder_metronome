f458e7b1ae57db2551a4a5305f6bb01029b2fa31
Jurijs Kastanovs <jurijs.kastanovs@vrsverige.com>
2026-03-12 07:54:50 +0200

    Dark splashscreen

--- app/src/main/java/com/recorder/MainActivity.kt
+++ app/src/main/java/com/recorder/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
     private val activityResultSender = ActivityResultSender(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
